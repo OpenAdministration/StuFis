6ae741e735ffe6118135f952fc127548521d4011
root <root@box.stura.tu-ilmenau.de>
2016-12-17 23:42:32 +0100

    php debugging

--- sendNewAntrag.php
+++ sendNewAntrag.php
@@ -1,11 +1,12 @@
 <?php
+echo "Hallo Welt0";
 $link = mysqli_connect("localhost","fvs","dkURw8yL5xx9f2na")  or die("failed to connect to server !!");
 mysqli_select_db($link,"finanzen_intern");
 if(isset($_REQUEST['submit']))
 {
 $errorMessage = "";
 $projekt_titel=$_POST['projekt-titel'];
-$projekt_institution = $_POST['von-pick']
+$projekt_institution = $_POST['von-pick'];
 $projekt_verantwortlich = $_POST['projekt-verantwortlich'];
 $projekt_beschluss=$_POST['projekt-beschluss']
 $date_von=$_POST['date-von'];
@@ -19,11 +20,14 @@ echo "<p class='message'>" .$errorMessage. "</p>" ;
 }
 else{
 //Inserting record in table using INSERT query
+print("Hallo Welt1");
 $insqDbtb="INSERT INTO `finanzen_intern`.`antraege`
 (`titel`, `orga`, `mail`, `link`, `begin`,
 `ende`, `beschreibung`) VALUES ('$projekt_titel', '$projekt_institution',
 '$projekt_verantwortlich', '$projekt_beschluss', '$date_von', '$date_bis', '$beschreibung')";
 mysqli_query($link,$insqDbtb) or die(mysqli_error($link));
 }
+print("Hallo Welt2");
 }
+print("Hallo Welt3");
 ?>
