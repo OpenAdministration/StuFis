a1862b001c2adc100a54418e58a7e5e81ed56553
lu_kors <lu_kors+github@posteo.de>
2017-01-09 16:09:49 +0100

    missing $

--- Backend.php
+++ Backend.php
@@ -94,8 +94,8 @@
 					$insStmt->execute();
 					foreach ($insStmt->fetchAll() as $row){
 						echo "<tr>";
-						for($i = 1; i <= count($row); $i++){
-							echo "<td>$row[i]</td>";
+						for($i = 1; $i <= count($row); $i++){
+							echo "<td>$row[$i]</td>";
 						}
 						echo "</tr>";
 					}
