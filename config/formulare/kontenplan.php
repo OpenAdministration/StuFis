e763c5b396dfba82771770b327b51b49633b877b
Lukas Staab <lukas@box.stura.tu-ilmenau.de>
2017-12-16 09:25:38 +0100

    missing for fix #47

--- config/formulare/kontenplan.php
+++ config/formulare/kontenplan.php
@@ -1,51 +1,52 @@
 <?php
 
 $config = [
-  "title" => "Kontenplan",
-  "shortTitle" => "Konten",
-  "state" => [ "draft"    => [ "Entwurf", "Entwurf", ],
-               "final"    => [ "In Kraft", "in Kraft" ],
-             ],
-  "createState" => "draft",
-  "categories" => [
-    "plan" => [
-      [],
+    "title" => "Kontenplan",
+    "shortTitle" => "Konten",
+    "state" => [ "draft"    => [ "Entwurf", "Entwurf", ],
+                "final"    => [ "In Kraft", "in Kraft" ],
+               ],
+    "createState" => "draft",
+    "categories" => [
+        "plan" => [
+            [],
+        ],
     ],
-  ],
-  "validate" => [
-    "postEdit" => [
-      [ "state" => "final", "requiredIsNotEmpty" => true ],
+    "canBeLinked" => ["state" => "final"],
+    "validate" => [
+        "postEdit" => [
+            [ "state" => "final", "requiredIsNotEmpty" => true ],
+        ],
     ],
-  ],
-  "permission" => [
-    /* each permission has a name and a list of sufficient conditions.
+    "permission" => [
+        /* each permission has a name and a list of sufficient conditions.
      * Each condition is an AND clause.
      * This is merged with form data that can add extra permissions not given here
      * hasPermission: true if all given permissions are present
      * group: true if all given groups are present
      * field: true if all given checks are ok
      */
-    "canBeLinked" => [
-      [ "state" => "final" ],
+        "canBeLinked" => [
+            [ "state" => "final" ],
+        ],
+        "canBeCloned" => true,
+        "canRead" => true,
+        "canEdit" => [
+            [ "group" => "ref-finanzen" ],
+        ],
+        "canDelete" => [
+            [ "state" => "draft", "hasPermission" => "canEdit" ],
+        ],
+        "canCreate" => [
+            [ "hasPermission" => [ "canEdit", "isCreateable" ] ],
+        ],
+        "canStateChange.from.draft.to.final" => [
+            [ "hasPermission" => "canEdit" ],
+        ],
     ],
-    "canBeCloned" => true,
-    "canRead" => true,
-    "canEdit" => [
-      [ "group" => "ref-finanzen" ],
+    "proposeNewState" => [
+        "draft" => [ "final" ],
     ],
-    "canDelete" => [
-      [ "state" => "draft", "hasPermission" => "canEdit" ],
-    ],
-    "canCreate" => [
-      [ "hasPermission" => [ "canEdit", "isCreateable" ] ],
-    ],
-    "canStateChange.from.draft.to.final" => [
-      [ "hasPermission" => "canEdit" ],
-    ],
-  ],
-  "proposeNewState" => [
-    "draft" => [ "final" ],
-  ],
 ];
 
 registerFormClass( "kontenplan", $config );
