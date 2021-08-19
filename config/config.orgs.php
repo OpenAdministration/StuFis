642200806426a014df79bfa38bb087a18b029bf5
Lukas Staab <14363325+lukas-staab@users.noreply.github.com>
2021-08-19 14:05:20 +0200

    Update config.orgs.php

--- config/config.orgs.php
+++ config/config.orgs.php
@@ -89,4 +89,92 @@ return [
             'hide-protokoll',
         ],
     ],
+     'sm' => [
+        'gremien' => [
+            'Zentral' => [
+                'StuRa Vorstand',
+                'Antidiskriminierungsstelle',
+                'Fachschaftsratsbeauftragter',
+                'Hochschulgruppenbeauftragter',
+            ],
+            'Referate' => [
+                'Referat Finanzen',
+                'Referat Hochschulpolitik',
+                'Referat Öffentlichkeitsarbeit',
+                'Referat Kultur',
+                'Referat Sozialpolitik und Soziales',
+                'Referat Internes',
+                'Referat Internationales',
+                'Referat Nachhaltigkeit',
+                'Referat Sport',
+            ],
+            'Philosophische Fakultät' => [
+                'FSR Anglistik',
+                'FSR Germanistik',
+                'FSR Geschichte',
+                'FSR KW',
+                'FSR Literaturwissenschaft',
+                'FSR Philosophie',
+                'FSR Religionswissenschaft',
+                'FSR Romanistik',
+            ],
+            'Erziehungswissenschaftliche Fakultät' => [
+                'FSR EW',
+                'FSR FSI',
+                'FSR Kunst',
+                'FSR Lehramt',
+                'FSR Musik',
+                'FSR PEB',
+                'FSR Pychologie',
+                'FSR Sport',
+                'FSR Technik',
+            ],
+            'Staatswissenschaftliche Fakultät' => [
+                'FSR StaWi',
+            ],
+            'Katholisch-Theologische Fakultät' => [
+                'FSR Kath. Theologie',
+            ],
+        ],
+        'rechtsgrundlagen' => [
+            "stura" => [
+                'label' => 'StuRa-Beschluss',
+                'label-additional' => 'Beschluss-Nr',
+                'placeholder' => '',
+                'hint-text' => '',
+            ],
+            "vorstand" => [
+                'label' => 'Vorstandsbeschluss',
+                'label-additional' => 'Beschluss-Nr',
+                'placeholder' => '',
+                'hint-text' => '',
+            ],
+            "fo-5a" => [
+                'label' => 'FO5a (Referate)',
+                'hint-text' => 'Interne Genehmigung durch Finanzreferenten',
+            ],
+            "fo-93a" => [
+                'label' => 'FO93b (FSRs durch Finanzreferat)',
+                'hint-text' => 'Externe Genehmigung durch Finanzreferat bis 250€',
+            ],
+            "fo-93b" => [
+                'label' => 'FO93b (Kleidung)',
+                'hint-text' => 'Externe Genehmigung durch Finanzreferat von Öffentlichkeitswirksamer Kleidung bis 510 bei max. 30 € pro Person',
+            ],
+            "fo-93c" => [
+                'label' => 'FO93b (Teambuilding)',
+                'hint-text' => 'Externe Genehmigung durch Finanzreferat von bis zu 75% der Kosten und max. 15€ pro Person',
+            ],
+        ],
+        'mailinglists' => [],
+        'impressum-url' => '',
+        'datenschutz-url' => '',
+        'issues-url' => '',
+        'help-url' => '',
+        'mail-domain' => 'uni-erfurt.de',
+        'finanzen-mail' => 'lukas.staab@tu-ilmenau.de',
+        'projekt-form' => [
+            'hide-protokoll',
+        ],
+    ],
 ];
