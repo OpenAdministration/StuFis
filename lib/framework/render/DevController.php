bb363e871451f79b57cb8d981e0d90737ec1da24
Lukas Staab <l.staab@posteo.de>
2022-04-12 01:55:43 +0200

    added dev action to delete session

--- lib/framework/render/DevController.php
+++ lib/framework/render/DevController.php
@@ -13,6 +13,13 @@ class DevController extends Renderer
         parent::__construct($routeInfo);
     }
 
+    public function actionKillSession(): void
+    {
+        session_destroy();
+        session_unset();
+        session_start();
+    }
+
     public function actionSession(): void
     {
         echo '<pre>' . var_export($_SESSION, true) . '</pre>';
@@ -23,7 +30,7 @@ class DevController extends Renderer
         echo '<pre>' . var_export(AuthHandler::getInstance()->getUserGroups(), true) . '</pre>';
     }
 
-    public function actionGremien() : void
+    public function actionGremien(): void
     {
         echo '<pre>' . var_export(AuthHandler::getInstance()->getUserGremien(), true) . '</pre>';
     }
@@ -36,14 +43,14 @@ class DevController extends Renderer
     public function actionFlicker()
     {
         echo '<pre>';
-        //var_dump($hexSol);
+        // var_dump($hexSol);
         // $challenge = "024 8A 01 2043801998 08 12345678";
-        //$challenge = "0388A01239230520422DE12500105170648489890"; // <- works
-        //$challenge = "038 8A 01 2392307899 22 DE12500105170648489890"; // <- works
+        // $challenge = "0388A01239230520422DE12500105170648489890"; // <- works
+        // $challenge = "038 8A 01 2392307899 22 DE12500105170648489890"; // <- works
         $challenge = '038 8A 01 2392302069 22 DE12500105170648489890'; // <- works
         $p = new FlickerGenerator($challenge);
-        //var_dump($p);
-        //var_dump(str_replace(' ', '', $hexSol));
+        // var_dump($p);
+        // var_dump(str_replace(' ', '', $hexSol));
         echo '</pre>';
         $svg = $p->getSVG(10, 300);
         echo PHP_EOL . $svg;
