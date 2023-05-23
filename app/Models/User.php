50553abdc3d8f18ae7f37ef36657f7e9e38696dc
Lukas Staab <l.staab@posteo.de>
2023-05-23 09:13:46 +0200

    ide helpers

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,51 @@ use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\GoogleProvider;
 use Spatie\LaravelIgnition\Exceptions\InvalidConfig;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $username
+ * @property string $email
+ * @property string $provider
+ * @property string $provider_sub
+ * @property string $provider_token
+ * @property string $provider_token_expiration
+ * @property string $provider_refresh_token
+ * @property string $provider_refresh_token_expiration
+ * @property string $picture_url
+ * @property string $iban
+ * @property string $address
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIban($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePictureUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProvider($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProviderRefreshToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProviderRefreshTokenExpiration($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProviderSub($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProviderToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProviderTokenExpiration($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUsername($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
