03a55b2c69051cb6042f995ec5b9d4fca71e2a37
Lukas Staab <l.staab@posteo.de>
2024-04-02 12:31:14 +0200

    [#110] Löschen von Projekten und Abrechnungen
    https://projekt.open-administration.de/work_packages/110

---
+++ app/Http/Controllers/Legacy/ProjectController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Legacy;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
