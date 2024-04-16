a96e16004528fc75246ffda2b28b97e6211dc8d1
Lukas Staab <l.staab@posteo.de>
2024-04-16 18:28:01 +0200

    throwing meaningful error page if no budget is given in project

--- lang/de/errors.php
+++ lang/de/errors.php
@@ -3,6 +3,10 @@
 return [
     '404.title' => 'Diese Seite existiert nicht :(',
     '404.subtitle' => 'Sorry! Wir konnten die Seite leider nicht finden...',
+
+    '420.title' => 'Anforderungsfehler',
+    '420.subtitle' => 'Das zugehörige Projekt hat keinen Projektposten welchem ein Titel zugeordnet ist. Die/der Haushaltsverantwortliche*r muss ihn eintragen.',
+
     'back-link' => 'Zurück',
     'doc-link.title' => 'Dokumentation',
     'doc-link.subtitle' => 'Lies unser Benutzer*innen Handbuch für mehr Infos',
