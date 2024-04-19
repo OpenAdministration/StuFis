f7abf019fab5ec40e1ec8c0fb673c958de834c77
Lukas Staab <l.staab@posteo.de>
2024-04-19 19:51:42 +0200

    [#255] autoupdate with backup
    https://projekt.open-administration.de/work_packages/255

--- config/app.php
+++ config/app.php
@@ -217,13 +217,13 @@ return [
         'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
-
     // app specific custom config
+    'version' => \Composer\InstalledVersions::getPrettyVersion('openadministration/stufis'),
     'about_url' => env('ABOUT_URL', 'https://open-administration.de/index.php/kontakt-und-impressum/'),
     'privacy_url' => env('PRIVACY_URL', 'https://open-administration.de/index.php/datenschutz/'),
     'terms_url' => env('TERMS_URL', 'https://open-administration.de/index.php/nutzungsbedingungen/'),
     'help_contact_mail' => env('HELP_CONTACT_MAIL', 'stufis@open-administration.de'),
-    'git-repo' => env('GIT_URL', 'https://github.com/openadministration/stufis'),
+    'git-repo' => env('GIT_URL', 'https://github.com/openadministration/stufis/releases'),
     'blog_url' => env('BLOG_URL', 'https://open-administration.de'),
     'docs_url' => env('DOCS_URL', 'https://doku.open-administration.de'),
 
