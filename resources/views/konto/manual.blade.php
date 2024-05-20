f9c8be27551c37f30512f90137bbf438799747c5
thsstll <till+github@isenhuth.de>
2024-05-20 15:35:51 +0200

    nicht gespeicherte wip vom csv import

--- resources/views/konto/manual.blade.php
+++ resources/views/konto/manual.blade.php
@@ -47,7 +47,9 @@
                         <div class="form-group">
                             <label for="choose-konto" class="block text-sm font-medium leading-6 text-gray-900">{{ __('konto.csv-label-choose-konto') }}</label>
                             <select id="choose-konto" name="choose-konto" class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-
+                                @foreach($data[0] as $csvheader => $value)
+                                    <option>{{ $csvheader }}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
