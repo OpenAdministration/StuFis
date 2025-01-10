7904c25c7db65ded07702dc7d0c8cfaed29217bd
Lukas Staab <l.staab@posteo.de>
2025-01-10 20:21:51 +0100

    cleanup testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,32 +8,6 @@ class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    public $baseUrl = 'http://localhost:8000';
+    public string $baseUrl = 'http://localhost:8000';
 
-    /**
-     * Indicates whether the default seeder should run before each test.
-     *
-     * @var bool
-     */
-    protected $seed = true;
-
-    protected function loginAsUser(): void
-    {
-        $this->loginAs('user');
-    }
-
-    protected function loginAsBudgetManager(): void
-    {
-        $this->loginAs('hv');
-    }
-
-    protected function loginAsCashManager(): void
-    {
-        $this->loginAs('kv');
-    }
-
-    protected function loginAs(string $username): void
-    {
-        $this->actingAs(User::where(['username' => $username])->first());
-    }
 }
