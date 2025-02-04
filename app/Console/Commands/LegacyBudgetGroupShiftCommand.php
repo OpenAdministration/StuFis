7c555e6e59aa3f63e96c9df1c3f8bcf09e9f554b
Lukas Staab <l.staab@posteo.de>
2025-02-04 14:56:53 +0100

    command: can shift hhp-group of the newest plan, so a new one can be added

---
+++ app/Console/Commands/LegacyBudgetGroupShiftCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Legacy\LegacyBudgetGroup;
+use App\Models\Legacy\LegacyBudgetPlan;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class LegacyBudgetGroupShiftCommand extends Command
+{
+    protected $signature = 'legacy:budget-group-shift
+        {new_group_id : The new Group ID}';
+
+    protected $description = 'Insert an new budget group with the given id in the newest BudgetPlan';
+
+    public function handle(): void
+    {
+        \DB::transaction(function () {
+            $latestPlan = LegacyBudgetPlan::all()->last();
+            $budgetGroups = LegacyBudgetGroup::where('hhp_id', $latestPlan->id)
+                ->where('id', '>=', $this->argument('new_group_id'));
+            $this->info('The following amount of other groups will be shifted back: '.$budgetGroups->count());
+            \Schema::disableForeignKeyConstraints();
+
+            $budgetGroups->each(function (LegacyBudgetGroup $budgetGroup) {
+                $budgetGroup->budgetItems()->update(['hhpgruppen_id' => DB::raw('hhpgruppen_id + 1')]);
+            });
+            // this is so hacky ...
+            $budgetGroups->update(['id' => DB::raw('-(id + 1)')]);
+            LegacyBudgetGroup::where('id', '<', 0)->update(['id' => DB::raw('-id')]);
+            // but its needed, otherwise there is a duplicate key. id's should not be used for sorting...
+            \Schema::enableForeignKeyConstraints();
+            $newGroup = new LegacyBudgetGroup([
+                'hhp_id' => $latestPlan->id,
+                'gruppen_name' => $this->ask('Please enter new Group name:'),
+                'type' => $this->ask('Is it Einname or Ausgabe? E/A') === 'E' ? 0 : 1,
+            ]);
+            $newGroup->id = $this->argument('new_group_id');
+            $newGroup->save();
+
+        });
+    }
+}
