d325ad33057aa87993834b0184c3cfc4110cdde0
Lukas Staab <l.staab@posteo.de>
2025-02-07 14:36:11 +0100

    demo: fix ci

--- database/seeders/DemoDataSeeder.php
+++ database/seeders/DemoDataSeeder.php
@@ -6,6 +6,8 @@ use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Process;
+use Storage;
 
 class DemoDataSeeder extends Seeder
 {
@@ -18,7 +20,7 @@ class DemoDataSeeder extends Seeder
             throw new \InvalidArgumentException('Realm is not demo but we are in production, aborting for your safety');
         }
 
-        $sqlContent = str(\Storage::disk('demo')->get('stufis-demo-data.sql'));
+        $sqlContent = str(Storage::disk('demo')->get('stufis-demo-data.sql'));
         $today = Carbon::today();
         $biggerYear = $today->year;
         if ($today->month < 4) {
@@ -28,13 +30,13 @@ class DemoDataSeeder extends Seeder
         $sqlContent = $sqlContent
             ->replace('2024', $biggerYear)
             ->replace('2023', $smallerYear);
-        if (App::runningUnitTests()) {
-            $sqlContent->replace('demo__', 'test__');
+        if (App::environment() === 'testing') {
+            $sqlContent = $sqlContent->replace('demo__', config('database.connections.mysql.prefix'));
         }
 
         DB::unprepared($sqlContent);
 
-        \Storage::delete('auslagen');
-        \Storage::copy(storage_path('demo/auslagen'), storage_path('app/auslagen'));
+        Storage::delete('auslagen');
+        Process::run(['cp', '-r', storage_path('demo/auslagen'), storage_path('app/')]);
     }
 }
