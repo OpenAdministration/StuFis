e0d6a2bebbd4aa6d573497c9f106a7a080c74069
Shift <shift@laravelshift.com>
2025-02-07 09:53:35 +0000

    Remove `createApplication` method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
 
     public string $baseUrl = 'http://localhost:8000';
 }
