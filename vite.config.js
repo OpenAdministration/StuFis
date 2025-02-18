ccb48a2b435924a96fcd96bd8122c0493c3e2f67
Lukas Staab <l.staab@posteo.de>
2025-02-18 14:24:13 +0100

    removed vue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -10,14 +9,6 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
         }),
     ],
 });
