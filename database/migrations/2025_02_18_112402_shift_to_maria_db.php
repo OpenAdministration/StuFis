fb12daf61ce246a8b06f68af8cd853b4214b3a23
Lukas Staab <l.staab@posteo.de>
2025-02-18 12:28:15 +0100

    shift to mariadb

---
+++ database/migrations/2025_02_18_112402_shift_to_maria_db.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            // create legacy is already changed, this is not for new setups but for upgrading instances < 4.3.x
+            $table->text('text')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {}
+};
