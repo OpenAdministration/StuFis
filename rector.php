170b294fbe232327a5aef962cda6d27b1181551c
Lukas Staab <l.staab@posteo.de>
2025-02-18 18:10:21 +0100

    introducing rector/rector

---
+++ rector.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/lang',
+        __DIR__.'/legacy',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets(php84: true);
+// ->withTypeCoverageLevel(1)
