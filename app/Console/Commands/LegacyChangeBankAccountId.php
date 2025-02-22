0c87a8aec6be92e2ac9c38dddc464c537416d314
Shift <shift@laravelshift.com>
2025-02-22 13:44:28 +0000

    Adopt Laravel type hints

--- app/Console/Commands/LegacyChangeBankAccountId.php
+++ app/Console/Commands/LegacyChangeBankAccountId.php
@@ -29,7 +29,7 @@ class LegacyChangeBankAccountId extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): int
     {
         return DB::transaction(function () {
             $oldId = $this->argument('old-id');
