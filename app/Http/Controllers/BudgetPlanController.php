0f4c40bb6f5f1796eac17298302c2226ba8e4003
Lukas Staab <l.staab@posteo.de>
2025-05-13 14:53:42 +0200

    using flux:toasts and more functionality. composer update and db changes

--- app/Http/Controllers/BudgetPlanController.php
+++ app/Http/Controllers/BudgetPlanController.php
@@ -18,4 +18,24 @@ class BudgetPlanController extends Controller
     {
         return view('budget-plan.show');
     }
+
+    public function create()
+    {
+        $plan = BudgetPlan::create(['state' => 'draft']);
+        $groups = $plan->budgetItems()->createMany([
+            ['is_group' => 1, 'budget_type' => 1, 'position' => 1, 'short_name' => 'A1'],
+            ['is_group' => 1, 'budget_type' => -1, 'position' => 1, 'short_name' => 'E1'],
+        ]);
+        $groups->each(function ($group) use ($plan) {
+            $group->children()->createMany([
+                [
+                    'budget_plan_id' => $plan->id,
+                    'is_group' => 0, 'position' => 1,
+                    'budget_type' => $group->budget_type,
+                ],
+            ]);
+        });
+
+        return redirect()->route('budget-plan.edit', ['plan_id' => $plan->id]);
+    }
 }
