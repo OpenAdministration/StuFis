6a290432c54c84ae98c27953fd9cf5e61974f45e
Lukas Staab <l.staab@posteo.de>
2025-07-09 00:38:07 +0200

    fix: font weights and using variable inter font

---
+++ resources/views/debug/font-weight.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <p class="font-thin">{{ $text }}</p>
+    <p class="font-extralight">{{ $text }}</p>
+    <p class="font-light">{{ $text }}</p>
+    <p class="font-normal">{{ $text }}</p>
+    <p class="font-medium">{{ $text }}</p>
+    <p class="font-semibold">{{ $text }}</p>
+    <p class="font-bold">{{ $text }}</p>
+    <p class="font-extrabold">{{ $text }}</p>
+    <p class="font-black">{{ $text }}</p>
+</x-layout>
