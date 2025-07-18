7f325911ff03e6721bc1df9c6b6c8835d678d304
Lukas Staab <l.staab@posteo.de>
2025-07-18 09:00:54 +0200

    refactoring page layout

--- app/View/Components/Layout.php
+++ app/View/Components/Layout.php
@@ -6,34 +6,6 @@ use Illuminate\View\Component;
 
 class Layout extends Component
 {
-    public string $title = 'StuFiS - Finanzen';
-
-    public string $version;
-
-    public array $profileSkeleton = [
-        [
-            'text' => 'Mein Profil',
-            'link' => '',
-        ],
-        [
-            'text' => 'Logout',
-            'route' => ['name' => 'logout'],
-        ],
-    ];
-
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct(/**
-     * @var string if not empty, the legacy iframe will be shown with this content, instead of the slot
-     */
-        public string $legacyContent = '')
-    {
-        $this->version = config('stufis.version');
-    }
-
     /**
      * Get the view / contents that represent the component.
      *
@@ -41,8 +13,6 @@ class Layout extends Component
      */
     public function render()
     {
-        return view('components.layouts.index', [
-            'version' => config('stufis.version'),
-        ]);
+        return view('components.layouts.index');
     }
 }
