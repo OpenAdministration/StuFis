6df204995459264551bde158d8a5bf8918d63d97
Lukas Staab <l.staab@posteo.de>
2025-07-25 11:42:32 +0200

    delete legacy budgetplan command

--- app/Console/Commands/LegacyDeleteBudgetPlan.php
+++ app/Console/Commands/LegacyDeleteBudgetPlan.php
@@ -13,32 +13,36 @@ class LegacyDeleteBudgetPlan extends Command
      *
      * @var string
      */
-    protected $signature = 'legacy:delete-hhp {id}';
+    protected $signature = 'legacy:refactor-hhp {{--transformation : space seperated list of x->y title ids or x-y }}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Deletes a given HHP';
+    protected $description = 'Deletes a given HHP - DANGER - deletes old one, upload over ui in the meantime and enter a transformation ';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $hhp = LegacyBudgetPlan::findOrFail($this->argument('id'));
-        $groups = $hhp->budgetGroups;
-        $title = LegacyBudgetItem::whereIn('hhpgruppen_id', $groups->pluck('id'));
+        $hhp = LegacyBudgetPlan::orderBy('id', 'desc')->firstOrFail();
+        $groups = $hhp->budgetGroups();
+        $title = LegacyBudgetItem::whereIn('hhpgruppen_id', $groups->pluck('id'))->keyBy('id');
         $this->info("Found {$title->count()} titles");
-        if ($this->confirm("Are you sure you want to delete HHP from {$hhp->von} to {$hhp->bis} in state {$hhp->state}?")) {
+        $this->warn('This operation cannot be rolled back and will be done without checking corresponding foreign keys');
+        if (! $this->confirm("Are you sure you want to delete HHP from {$hhp->von} to {$hhp->bis} in state {$hhp->state}?")) {
             return;
         }
 
         \DB::transaction(function () use ($hhp, $groups, $title) {
+            \Schema::disableForeignKeyConstraints();
             $title->delete();
             $groups->delete();
             $hhp->delete();
+            \Schema::enableForeignKeyConstraints();
+            $this->info('Plan, Groups and Bugets are deleted successfully!');
         });
 
     }
