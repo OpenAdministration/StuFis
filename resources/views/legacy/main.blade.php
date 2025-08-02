82e34fc6ffa05aa5f6cc9e6ed2a7e01557e8300e
Lukas Staab <l.staab@posteo.de>
2025-08-02 13:32:34 +0200

    fix: iframes need closing tags - debugbar not visible

--- resources/views/legacy/main.blade.php
+++ resources/views/legacy/main.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
     <iframe srcdoc="{!! htmlspecialchars($content) !!}"
-            width="100%" height="100%" id="legacy-iframe"/>
+            width="100%" height="100%" id="legacy-iframe"></iframe>
 </x-layout>
