7864461631d7d694f9041f4c54e924d523c826c6
Lukas Staab <l.staab@posteo.de>
2025-08-02 13:30:58 +0200

    fixup legacy route group middelwares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,14 @@
 <?php
 
-use App\Providers\AppServiceProvider;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\VersionChangeNotification;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders([
-        \SocialiteProviders\Manager\ServiceProvider::class,
-    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
@@ -41,27 +40,20 @@ return Application::configure(basePath: dirname(__DIR__))
             }
 
             // has to be last because there is a catch-all inside
-            Route::middleware('legacy')
+            Route::middleware('web')
+                ->withoutMiddleware(VerifyCsrfToken::class)
                 ->group(base_path('routes/legacy.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->redirectUsersTo(AppServiceProvider::HOME);
+        $middleware->redirectUsersTo('/');
 
         $middleware->throttleApi();
 
-        $middleware->group('legacy', [
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
-            \Illuminate\Http\Middleware\HandleCors::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ]);
-
         $middleware->alias([
-            'auth' => \App\Http\Middleware\Authenticate::class,
+            'auth' => Authenticate::class,
         ]);
+        $middleware->appendToGroup('web', VersionChangeNotification::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    \SocialiteProviders\Manager\ServiceProvider::class,
 ];
