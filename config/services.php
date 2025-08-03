e9a47098b5a0a15ee281c2090ed3aeabbb9b0594
Lukas Staab <l.staab@posteo.de>
2025-08-03 18:23:47 +0200

    thrown out old unused services

--- config/services.php
+++ config/services.php
@@ -2,21 +2,6 @@
 
 return [
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
-    ],
-
-    'keycloak' => [
-        'client_id' => env('KEYCLOAK_CLIENT_ID'),
-        'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
-        'redirect' => env('KEYCLOAK_REDIRECT_URI'),
-        'base_url' => env('KEYCLOAK_BASE_URL'),   // Specify your keycloak server URL here
-        'realms' => env('KEYCLOAK_REALM'),         // Specify your keycloak realm
-    ],
-
     'stumv' => [
         // using socialiteproviders/laravelpassport
         'client_id' => env('STUMV_CLIENT_ID'),
