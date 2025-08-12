93d481b9ed09130f4ddc2962dd56770d0ad74008
Lukas Staab <l.staab@posteo.de>
2025-08-12 11:19:33 +0200

    lang

--- lang/de/general.php
+++ lang/de/general.php
@@ -1,6 +1,19 @@
 <?php
 
 return [
+    'new-project-button' => 'Neues Projekt',
+    'breadcrumb.todo' => 'TODO',
+    'breadcrumb.booking' => 'Buchungen anweisen',
+    'breadcrumb.booking.text' => 'Buchungen durchführen',
+    'breadcrumb.booking.history' => 'Buchungsliste / Historie',
+    'breadcrumb.konto' => 'Konto/Kasse',
+    'breadcrumb.konto.new' => 'Neues Konto oder neue Kasse anlegen',
+    'breadcrumb.konto.import.csv' => 'CSV-Import',
+    'breadcrumb.konto.credentials' => 'Bankimport',
+    'breadcrumb.konto.credentials.new' => 'Neuen Zugang anlegen',
+    'breadcrumb.sitzung' => 'Sitzung',
+    'breadcrumb.budget-plan' => 'Haushaltsplan',
+    'breadcrumb.hhp-title-details' => 'Haushaltstitel Details',
     'changelog.headline' => 'Changelog',
     'changelog.sub-headline' => 'Die wichtigsten Änderungen findet ihr hier in gekürzter Form auf Deutsch. Eine ausführlichere Liste alle Anpassungen an StuFiS findet ihr auf Github in Englisch.',
     'version_notification.text' => 'StuFiS wurde erfolgreich upgedatet. Mehr Informationen zu den Änderungen findest du unten links, wenn du auf die Versionsnummer klickst.',
