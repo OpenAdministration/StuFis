da45893a96f376336852c447237b210c3c1cdcf4
Lukas Staab <l.staab@posteo.de>
2025-12-02 13:35:28 +0100

    small rector fix, dont rector cache

--- rector.php
+++ rector.php
@@ -22,7 +22,8 @@ return RectorConfig::configure()
     ])
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/bootstrap/providers.php',
         __DIR__.'/config',
         __DIR__.'/lang',
         // __DIR__.'/legacy',
