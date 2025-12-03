72347ad3c7be56291cbe9d9f7d373c59317963e9
Lukas Staab <l.staab@posteo.de>
2025-12-04 00:44:46 +0100

    changed default timezone to Europe/Berlin

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Europe/Berlin'),
 
     /*
     |--------------------------------------------------------------------------
