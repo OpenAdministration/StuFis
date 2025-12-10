ebd0d7bdf34a5e13b808fe69d755419ec1a8f81a
Lukas Staab <l.staab@posteo.de>
2025-12-10 09:04:29 +0100

    fix: correct swapped values for ChatMessageType enum SYSTEM and PUBLIC

--- app/Models/Enums/ChatMessageType.php
+++ app/Models/Enums/ChatMessageType.php
@@ -4,9 +4,9 @@ namespace App\Models\Enums;
 
 enum ChatMessageType: int
 {
-    case SYSTEM = 0;
+    case SYSTEM = 1;
 
-    case PUBLIC = 1;
+    case PUBLIC = 0;
     case SUPPORT = 2;
     case FINANCE = 3;
 
