b0b2c0c19c40e6d71511673e10c75b986d54b9d1
Lukas Staab <l.staab@posteo.de>
2026-06-19 10:52:40 +0200

    fix project: daterange can be 1 day only

--- app/States/Project/ProjectState.php
+++ app/States/Project/ProjectState.php
@@ -117,7 +117,7 @@ abstract class ProjectState extends State implements Wireable
             'org' => 'required|string|max:64',
             'protocol' => 'sometimes|nullable|string|url',
             'date_start' => 'required|date',
-            'date_end' => 'required|date|after:date_start',
+            'date_end' => 'required|date|after_or_equal:date_start',
             'beschreibung' => ['required', 'string', new FluxEditorRule],
             'posts' => 'required|array|min:1',
             'posts.*.id' => 'sometimes|integer',
