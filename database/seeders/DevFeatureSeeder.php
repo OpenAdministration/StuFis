512836358f8fd5daec7bb2946e8d46760b129e82
Lukas Staab <l.staab@posteo.de>
2026-07-08 17:17:17 +0200

    refactor(seeding): fixup seeding for the current migrations

--- database/seeders/DevFeatureSeeder.php
+++ database/seeders/DevFeatureSeeder.php
@@ -2,17 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\FiscalYear;
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DevFeatureSeeder extends Seeder
 {
-    public function run(): void
-    {
-        FiscalYear::create([
-            'start_date' => Carbon::today(),
-            'end_date' => Carbon::today()->addYear()->subDay(),
-        ]);
-    }
+    public function run(): void {}
 }
